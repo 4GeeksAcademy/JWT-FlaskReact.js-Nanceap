c26dae6d8f6e1e4272dd796db4cf905e5094f579
Nancy Acevedo <alepietri00@gmail.com>
2025-08-03 19:42:30 +0000

    login fix

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../dist/')
@@ -15,6 +16,8 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 CORS(app, supports_credentials=True)
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "jwt-secret-key") 
+jwt = JWTManager(app)  
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
@@ -25,11 +28,11 @@ else:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv("FLASK_APP_KEY", "super-secret")
 
-
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 setup_admin(app)
 setup_commands(app)
+
 app.register_blueprint(api, url_prefix="/api")
 
 @app.errorhandler(APIException)
@@ -53,6 +56,7 @@ def serve_any_other_file(path):
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+
 
 
 
